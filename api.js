f802e5dcadf0830643e9ff729459294294b96b5b
Chai Chung Yee <chungyee@fomatictech.com>
2024-12-03 15:50:28 +0800

    1. replace gethostvans with firebase 2. disable autocomplete in login page

--- api.js
+++ api.js
@@ -1,5 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  query,
+  where,
+} from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCxI4Oi9lBTTXBASUdmlw0uedlisJEwfqU",
@@ -7,65 +15,59 @@ const firebaseConfig = {
   projectId: "vanslife-32403",
   storageBucket: "vanslife-32403.firebasestorage.app",
   messagingSenderId: "645628155411",
-  appId: "1:645628155411:web:c601444948fd9044e951e6"
+  appId: "1:645628155411:web:c601444948fd9044e951e6",
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
 
-const vansCollectionRef = collection(db, "vans")
+const vansCollectionRef = collection(db, "van");
 
 export async function getVans() {
-    const querySnapshot = await getDocs(vansCollectionRef)
-    const dataArr = querySnapshot.docs.map(doc => ({
-        ...doc.data(),
-        id: doc.id
-    }))
-    console.log(dataArr)
-    return dataArr
+  const querySnapshot = await getDocs(vansCollectionRef);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+
+  return dataArr;
 }
 
-// export async function getVans(id) {
-//     const url = id ? `/api/vans/${id}` : "/api/vans"
-//     const res = await fetch(url)
-//     if (!res.ok) {
-//         throw {
-//             message: "Failed to fetch vans",
-//             statusText: res.statusText,
-//             status: res.status
-//         }
-//     }
-//     const data = await res.json()
-//     return data.vans
-// }
+export async function getVan(id) {
+  const docRef = doc(db, "van", id);
+  const vanSnapshot = await getDoc(docRef);
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+  return {
+    ...vanSnapshot.data(),
+    id: vanSnapshot.id,
+  };
+}
+
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"));
+  const querySnapshot = await getDocs(q);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+
+  return dataArr;
 }
 
 export async function loginUser(creds) {
-    const res = await fetch("/api/login",
-        { method: "post", body: JSON.stringify(creds) }
-    )
-    const data = await res.json()
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
 
-    if (!res.ok) {
-        throw {
-            message: data.message,
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
 
-    return data
+  return data;
 }
--- pages/Login.jsx
+++ pages/Login.jsx
@@ -43,6 +43,7 @@ export default function Login() {
                 method="post" 
                 className="login-form" 
                 replace
+                autocomplete="off"
             >
                 <input
                     name="email"
